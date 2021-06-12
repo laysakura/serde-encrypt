b93018f0a9071bdd278c47844e50c6892fa71904
Sho Nakatani <lay.sakura@gmail.com>
2021-06-13 07:44:33 +0900

    test: adds test case to check std error

---
+++ tests/feat_serde_encrypt_public_key_std_error.rs
@@ -0,0 +1,16 @@
+//! serde_encrypt::error::Error implements std::error::Error in `std` feature.
+//! (This test crate is not no_std)
+
+mod test_util;
+
+// https://rust-lang.github.io/api-guidelines/interoperability.html#error-types-are-meaningful-and-well-behaved-c-good-err
+#[test]
+fn test_api_guidelines_c_good_err() {
+    use std::fmt::Display;
+
+    fn assert_error<T: std::error::Error + Send + Sync + 'static>() {}
+    assert_error::<serde_encrypt::error::Error>();
+
+    fn assert_display<T: Display>() {}
+    assert_display::<serde_encrypt::error::Error>();
+}
