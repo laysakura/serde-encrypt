53878a22d858823d474e969d26fa9b5cf9669a55
Sho Nakatani <lay.sakura@gmail.com>
2021-06-15 16:29:11 +0900

    chore: -core lib.rs comment and dev-dependencies

--- serde-encrypt-core/src/lib.rs
+++ serde-encrypt-core/src/lib.rs
@@ -1,6 +1,9 @@
-//! serde-encrypt encrypts/decrypts any `strct`s and `enum`s that implements `serde::{Serialize, Deserialize`}.
+//! Core implementation of serde-encrypt crate.
 //!
-//! See [README.md](https://github.com/laysakura/serde-encrypt) for basic usage and implementation details.
+//! This crate is serde agnostic because:
+//!
+//! - serde cannot be easily build in some environment (e.g. rust-sgx-sdk),
+//! - so it is nice to manage serde dependency in thin layer (serde-encrypt crate here) in order to build core logic easily.
 
 #![deny(missing_debug_implementations, missing_docs)]
 #![cfg_attr(not(feature = "std"), no_std)]
