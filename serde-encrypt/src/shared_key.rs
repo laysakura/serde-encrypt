7f9f835140979bc9536eddfd5c1c15e2cca299c3
Kuriko Moe <kurikomoe@gmail.com>
2022-04-12 22:52:51 +0800

    fix(pub): fix missing `pub` on SharedKey field
    Fix the missing `pub` on `SharedKey` field.
    Add From<[u8;32]> trait for SharedKey.

--- serde-encrypt/src/shared_key.rs
+++ serde-encrypt/src/shared_key.rs
@@ -1,8 +1,8 @@
 //! serde-serializable shared key.
 
-use crate::{random::RngSingletonImpl, AsSharedKey};
 use serde::{Deserialize, Serialize};
 
+use crate::{AsSharedKey, random::RngSingletonImpl};
 use crate::traits::SerdeEncryptPublicKey;
 
 /// 32-byte key shared among sender and receiver secretly.
@@ -10,7 +10,13 @@ use crate::traits::SerdeEncryptPublicKey;
 /// It is a good practice to use [SerdeEncryptPublicKey](crate::traits::SerdeEncryptPublicKey)
 /// to exchange this shared key.
 #[derive(Clone, Eq, PartialEq, Hash, Debug, Serialize, Deserialize)]
-pub struct SharedKey([u8; 32]);
+pub struct SharedKey(pub [u8; 32]);
+
+impl From<[u8; 32]> for SharedKey {
+    fn from(data: [u8; 32]) -> Self {
+        Self(data)
+    }
+}
 
 impl AsSharedKey for SharedKey {
     type R = RngSingletonImpl;
@@ -35,5 +41,22 @@ cfg_if::cfg_if! {
         impl SerdeEncryptPublicKey for SharedKey {
             type S = PostcardSerializer<Self>;
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn build_sharedkey_from_array() {
+        const STATIC_ARRAY: [u8; 32] = [1, 1, 4, 5, 1, 4,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+        const SHAREDKEY_CONST: SharedKey = SharedKey(STATIC_ARRAY);
+
+        let shared_key = SharedKey::from(STATIC_ARRAY);
+
+        assert_eq!(shared_key, SHAREDKEY_CONST);
     }
 }
